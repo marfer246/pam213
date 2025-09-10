7d859c217f75e01ba00945700bcf5377053626f6
marfer246 <124049489@upq.edu.mx>
2025-09-10 00:04:59 -0600

    banderas

--- jsParaPrincipiantes/java.js
+++ jsParaPrincipiantes/java.js
@@ -1,41 +1,53 @@
-//tipos de datos 
-"Hello World" // string
-'Hello World' // string
+//variables
+var nombre = "john";
+length lastname = "john";
 
-1000000 // number
--2.3
+nombre = "yoyo";
+4  nombre e';
+6  const PI = 3.1415;
+7  PI = 100;
+9  console.log(PI)
 
-// boolean
-true
-false
-
-// array
-['joe', 'ryan', 'martha']
-[1, 2, 3]
-[true, false, true, false]
-
-
-
-// object
-{
-  "username": 'ryan',
-  "score": 70.4,
-  "hours": 14,
-  "proffesional": true
-}
-
-{
-  "username": 'joe',
-  "score": 0.4,
-  "hours": 1,
-  "proffesional": false
-}
-
-
-console.log("esto es una cadena")
-console.log("14.3")
-console.log("true")
-console.log(["1,2,3"])
+console.log(nombre);
+4  nameuser = 'pepe';
+6  const PI = 3.1415;
+7  PI = 100;
+9  console.log(PI)
  
-/un objeto empieza con llaves
-console.log({"username": "ryan", "score": 70.4});
+// camelcase
+let nombre = "gordon"; 
+
+//operadores
+
+let numberOne = 60;
+let numberTwo = 100;
+
+let result = numberOne | numberTwo;
+
+
+// ++++++++
+console.log(result);
+
+let numberOne = 100;
+
+let name = 'trala';
+let lastname = 'Carter';
+
+let completeName = name + ' ' + lastname;
+
+//console.log(completeName);
+//concatenar 
+
+let numberOne = 100;
+let numberTwo = 500;
+
+let result = numberOne > numberTwo;
+
+//console.log(result);
+
+let passwordDB = 'pepe123';
+let input = 'pepe123';
+
+let result = input == passwordDB;
+
+console.log(result);
