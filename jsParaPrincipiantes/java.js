219610fe0825a9b77e5d72ec4c78c9a2ff22e9f1
marfer246 <124049489@upq.edu.mx>
2025-09-10 00:18:57 -0600

    mayor y menor. var

--- jsParaPrincipiantes/java.js
+++ jsParaPrincipiantes/java.js
@@ -1,53 +1,13 @@
-//variables
-var nombre = "john";
-length lastname = "john";
+//condicional
 
-nombre = "yoyo";
-4  nombre e';
-6  const PI = 3.1415;
-7  PI = 100;
-9  console.log(PI)
+let score = 70;
 
-console.log(nombre);
-4  nameuser = 'pepe';
-6  const PI = 3.1415;
-7  PI = 100;
-9  console.log(PI)
- 
-// camelcase
-let nombre = "gordon"; 
+if (score > 30) {
+    console.log('You need to practice more');
+} else if (score > 15) {
+    console.log('Estas mejorando');
+}
 
-//operadores
-
-let numberOne = 60;
-let numberTwo = 100;
-
-let result = numberOne | numberTwo;
-
-
-// ++++++++
-console.log(result);
-
-let numberOne = 100;
-
-let name = 'trala';
-let lastname = 'Carter';
-
-let completeName = name + ' ' + lastname;
-
-//console.log(completeName);
-//concatenar 
-
-let numberOne = 100;
-let numberTwo = 500;
-
-let result = numberOne > numberTwo;
-
-//console.log(result);
-
-let passwordDB = 'pepe123';
-let input = 'pepe123';
-
-let result = input == passwordDB;
-
-console.log(result);
+else {
+    console.log('You need to follow this tutorial');
+}
