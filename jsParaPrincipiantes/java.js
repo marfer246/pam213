73a3861286e733a1c3e3f17e4e3542a08b89c054
marfer246 <124049489@upq.edu.mx>
2025-09-09 23:36:04 -0600

    tipos de datos
    los tipos de datos en la consola, nos ayuda a ver como funciona

--- jsParaPrincipiantes/java.js
+++ jsParaPrincipiantes/java.js
@@ -1,4 +1,41 @@
-console.log("hola mundo");
-document.writeln("Hola mundo");
-console.error("Mi error customizado");
-console.error("la aplicacion a fallado");
+//tipos de datos 
+"Hello World" // string
+'Hello World' // string
+
+1000000 // number
+-2.3
+
+// boolean
+true
+false
+
+// array
+['joe', 'ryan', 'martha']
+[1, 2, 3]
+[true, false, true, false]
+
+
+
+// object
+{
+  "username": 'ryan',
+  "score": 70.4,
+  "hours": 14,
+  "proffesional": true
+}
+
+{
+  "username": 'joe',
+  "score": 0.4,
+  "hours": 1,
+  "proffesional": false
+}
+
+
+console.log("esto es una cadena")
+console.log("14.3")
+console.log("true")
+console.log(["1,2,3"])
+ 
+/un objeto empieza con llaves
+console.log({"username": "ryan", "score": 70.4});
