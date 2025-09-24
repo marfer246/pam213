fb77b7b134b1aeccbb76a027d9d24e6098cd06fb
marfer246 <124049489@upq.edu.mx>
2025-09-23 23:21:21 -0600

    ejercicio 3

--- practica2/objetos.js
+++ practica2/objetos.js
@@ -1,11 +1,16 @@
-const productos=[
-    {nombre: "laptop", precio: 12000 },
-     {nombre: "mous", precio: 250 },
-      {nombre: "teclado", precio: 750 },
-      {nombre: "monitor", precio: 3000 }
+//1. Usa .find() para buscar a la persona con nombre "Luis".
+//2. Usa .forEach() para imprimir el nombre de cada persona con su edad.
+//3. Usa .reduce() para sumar todas las edades y obtener un total.
+const personas = [
+  { nombre: "Ana", edad: 22 },
+  { nombre: "Luis", edad: 35 },
+  { nombre: "María", edad: 28 }
 ];
-//console.log(productos);
-const nombres = productos
-.filter (producto => producto.precio > 1000)
-.map (producto => producto. nombre);
-console.log(nombres);
+
+const personaLuis =personas.find(persona =>persona.nombre === "Luis");
+console.log(personaLuis); 
+personas.forEach(persona => {
+  console.log(persona.nombre + " tiene " + persona.edad + " años");
+});
+const totalEdades= personas.reduce((acum, persona)=> acum+ persona.edad, 0);
+console.log("La suma total de las edades es:" +totalEdades +" años");
