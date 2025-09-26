476dce1cb86da876667c5577369db9fbce413dba
marfer246 <124049489@upq.edu.mx>
2025-09-25 22:31:04 -0600

    ejercicio B

--- practica3/main.js
+++ practica3/main.js
@@ -3,3 +3,27 @@ import { restar} from './utils.js';
 console.log(restar( 32, 13));
 console.log(restar( 22, 17));
 
+// b ------
+function verificarUsuario(usuario) {
+    //promesa
+    return new Promise((resolve,reject) =>{
+        if (usuario == "admin") {
+            resolve("Acceso concedido");
+        }else{
+            reject("Acceso denegado");
+        }
+    }); //ok 
+}
+verificarUsuario("admin")
+    .then(res=>console.log(res))
+    .catch(err=>console.error(err));
+verificarUsuario("ivan")
+    .then(res=> console.log(res))
+    .catch(err=>console.error(err));
+
+
+
+
+
+
+
