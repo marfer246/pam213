1cfbf716f1fe0440f53f4d3be6189c9d985c1596
marfer246 <124049489@upq.edu.mx>
2025-11-06 15:08:52 -0600

    avance

--- practica2/Examen2do/screens/MenuScreen.js
+++ practica2/Examen2do/screens/MenuScreen.js
@@ -1,23 +1,38 @@
-import { StyleSheet, View, Button } from 'react-native';
-import React from 'react';
 
-export default function MenuScreen() {
-  return (
-    <View style={styles.container}>
-      <Button
-        title="Pract: TextInput"
-        onPress={() => console.log('Botón presionado')}
-        color="#2196F3" // Azul compatible
-      />
-    </View>
-  );
+import { Text, StyleSheet, View, Button } from 'react-native'
+import React, { useState } from 'react'
+import ExameScreen from './ExameScreen'
+
+export default function MenuScreen (){
+  
+    const[ screen, setScreen ]= useState( 'menu'); 
+
+    switch(screen){
+
+        case 'ExamenScreen':
+            return <ExameScreen/>;
+
+
+        case 'menu':
+
+
+            default:
+                return (
+                    <View style = {style.container}>
+                        <Text style= {{ marginBottom: 20, fontSize: 30}}>Menu de practicas </Text>
+
+                         <Button color="pink" title='Pract: Examen' onPress={()=> setScreen('ExameScreen')}/>
+                        
+                    </View>
+                );
+    }
 }
 
-const styles = StyleSheet.create({
+const style = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent:  'center',
     alignItems: 'center',
-    padding: 20,
-  },
+},
 });
+
